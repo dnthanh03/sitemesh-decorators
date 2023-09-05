d834411a59388e752da4bad6fcccf1746f279b96
KHANHLINH03\MMC <linhntkph26829@fpt.edu.vn>
2023-09-05 21:57:26 +0700

    Commit 1

---
+++ src/main/java/com/laptrinhjavaweb/controller/LoginController.java
@@ -0,0 +1,11 @@
+package com.laptrinhjavaweb.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginController {
+
+    public String login(){
+        return "login";
+    }
+}
